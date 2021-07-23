e6e7f09a986b42ddf54103abed54833833a2688e
coderwhy <coderwhy@gmail.com>
2021-07-23 21:56:25 +0800

    style(login): coderwhy login style

--- src/App.vue
+++ src/App.vue
@@ -4,23 +4,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
 
 export default defineComponent({
   name: 'App',
-
-
-
-
   components: {
     HelloWorld
-
-
-
-
-
-
   }
 })
 </script>
